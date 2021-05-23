8c7e10c6941e00a0cbea9078bccc6b4db131e417
Chris Daems <daemschris@gmail.com>
2021-05-23 20:16:56 +0200

    styled components

---
+++ src/components/Coin/Coin.jsx
@@ -0,0 +1,72 @@
+
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Td = styled.td `
+    border: 1px solid #cccccc;
+    width: 25vh;
+    color: white;
+`;
+
+export default class Coin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            price: this.props.price 
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    /*
+    componentDidMount() {
+        const callback = () => {
+            // set the state to a new random value
+            const randomPercentage = 1.0 + Math.random() / 0.01;
+            // don't do this, the state can only be set in the constructor
+            //this.state.price = this.state.price * randomPercentage;
+            
+            // DO THIS instead 
+            this.setState( function(oldState) {
+                return {
+                    price: oldState.price * randomPercentage
+                }
+            })
+        }
+        setInterval( callback, 1000);
+    }
+    */
+    handleClick(event) {
+        // prevent the default action of submitting the form.
+        event.preventDefault();
+
+        const randomPercentage = 0.995 + Math.random() * 0.01;
+        this.setState( function(oldState) {
+            return {
+                price: oldState.price * randomPercentage
+            }
+        });
+
+    }
+    render() {
+        return (
+            <tr>
+                <Td>{this.props.name}</Td>
+                <Td>{this.props.ticker}</Td>
+                <Td>${this.state.price}</Td>
+                <Td>
+                    <form>
+                        <button onClick={this.handleClick} >Refresh</button>
+                    </form>
+                    
+                </Td>
+            </tr>
+        )
+    }
+}
+
+Coin.propTypes = {
+    name: propTypes.string.isRequired,
+    ticker: propTypes.string.isRequired,
+    price: propTypes.number.isRequired
+}
+
---
+++ src/components/ExchangeHeader/ExchangeHeader.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import logo from './logo.svg';
+
+export default class ExchangeHeader extends Component {
+    render() {
+        return (
+            <header className="App-header">
+                <img src={logo} alt="React logo" className='App-logo' />
+                <h1 className='App-title'>
+                    Coin Exchange
+                </h1>
+            </header>
+        )
+    }
+}
